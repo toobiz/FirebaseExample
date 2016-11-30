f193e45a4d897ac8573715475a48a672fe196ad3
Mike Tubis <michal.tubis@weblify.co>
2016-11-30 22:07:29 +0100

    Activities rearangement

--- app/src/main/java/com/example/mike/firebaseexample/ProfileActivity.java
+++ app/src/main/java/com/example/mike/firebaseexample/ProfileActivity.java
@@ -1,13 +1,10 @@
 package com.example.mike.firebaseexample;
 
-import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.graphics.Bitmap.Config;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.DataSnapshot;
@@ -15,54 +12,28 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
-    private EditText editTextName;
-    private EditText editTextAddress;
     private TextView textViewPersons;
     private Button buttonSave;
 
-    //        DatabaseReference.setAndroidContext(this);
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference ref = database.getReference("users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_profile);
 
         buttonSave = (Button) findViewById(R.id.buttonSave);
-//        editTextName = (EditText) findViewById(R.id.editTextName);
-//        editTextAddress = (EditText) findViewById(R.id.editTextAddress);
 
         textViewPersons = (TextView) findViewById(R.id.textViewPersons);
 
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Creating firebase object
-//                ContactsContract.Data ref = new DatabaseReference(Config.FIREBASE_URL);
-
-//                //Getting values to store
-//                String name = editTextName.getText().toString().trim();
-//                String address = editTextAddress.getText().toString().trim();
-//
-//                //Creating Person object
-//                Person person = new Person();
-//
-//                //Adding values
-//                person.setName(name);
-//                person.setAddress(address);
-
-                //Storing values to firebase
-//                ref.child("Person").setValue(person);
-
 
                 //Value event listener for realtime data update
                 ref.addValueEventListener(new ValueEventListener() {
--- app/src/main/java/com/example/mike/firebaseexample/SignupActivity.java
+++ app/src/main/java/com/example/mike/firebaseexample/SignupActivity.java
@@ -1,7 +1,18 @@
 package com.example.mike.firebaseexample;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+
+import android.util.Log;
 
 /**
  * Created by mike on 30.11.2016.
@@ -9,10 +20,25 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SignupActivity extends AppCompatActivity {
 
+    private Button buttonSave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("myTag", "Signup!");
+
+            }
+        });
     }
+
+
 
 }
