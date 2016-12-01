08bda8fe3f7a9fb291d08fd7efe5c8eb97f4ce68
Mike Tubis <michal.tubis@weblify.co>
2016-12-01 16:34:38 +0100

    create hard-coded user

---
+++ app/src/main/java/com/example/mike/firebaseexample/Config.java
@@ -0,0 +1,13 @@
+package com.example.mike.firebaseexample;
+
+/**
+ * Created by mike on 01.12.2016.
+ */
+
+public class Config {
+    public static final String BASE_URL = "https://bg-stage.firebaseio.com/";
+    public static final String USERS_URL = "https://bg-stage.firebaseio.com/users/";
+    public static final String MESSAGES_URL = "https://bg-stage.firebaseio.com/messages";
+    public static final String GAMES_URL = "https://bg-stage.firebaseio.com/games";
+    public static final String LOCATIONS_URL = "https://bg-stage.firebaseio.com/locations";
+}
--- app/src/main/java/com/example/mike/firebaseexample/SignupActivity.java
+++ app/src/main/java/com/example/mike/firebaseexample/SignupActivity.java
@@ -1,12 +1,19 @@
 package com.example.mike.firebaseexample;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.content.Intent;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.FirebaseDatabase;
@@ -14,6 +21,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.ValueEventListener;
 
 import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by mike on 30.11.2016.
@@ -21,25 +29,47 @@ import android.util.Log;
 
 public class SignupActivity extends AppCompatActivity {
 
+    private EditText editTextName;
+    private EditText editTextEmail;
     private Button buttonSave;
     private Button buttonSignin;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
         buttonSave = (Button) findViewById(R.id.buttonSave);
         buttonSignin = (Button) findViewById(R.id.sign_in_button);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("Sign in state", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("Sign in state", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
 
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                Log.d("myTag", "Signup!");
-
-                Intent profileIntent = new Intent(SignupActivity.this, ProfileActivity.class);
-                startActivity(profileIntent);
+                createUser("dupa@dupa.pl", "password");
             }
         });
 
@@ -52,5 +82,14 @@ public class SignupActivity extends AppCompatActivity {
     }
 
 
+    private void createUser(String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
 
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        Log.d("Hello!", "Hellog");
+                    }
+                });
+    }
 }
